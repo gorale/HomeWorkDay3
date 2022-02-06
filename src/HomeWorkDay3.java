ded1b31e934b245d77a3bfc1b36dbad04e2b319f
Gor <gor.aleksanyan.1197@gmail.com>
2022-02-06 22:26:40 +0400

    Last Updates

--- src/HomeWorkDay3.java
+++ src/HomeWorkDay3.java
@@ -2,7 +2,123 @@
 import java.util.Scanner;
 
 public class HomeWorkDay3 {
+    /**
+     * Swap Numbers
+     * @author Gor Aleksanyan
+     * */
+    public static void swapNumbers(){
+        Scanner num = new Scanner(System.in);
+        System.out.print("Enter number a: ");
+        int a = num.nextInt();
+        System.out.println("a: "+a);
+        System.out.print("Enter number b: ");
+        int b = num.nextInt();
+        System.out.println("b: "+b);
+        b = b+a;
+        a=b-a;
+        b = b-a;
+        System.out.println("a after swapping: "+a);
+        System.out.println("b after swapping: "+b);
+    }
+    /**
+     * Sting problem
+     * @author Gor Aleksanyan
+     * */
+    public static void stringProblem(){
+//        String text = "Vikings";
+        String txt = "We are the\r so-called \"Vikings\" \n from the";
+        System.out.println(txt);
+    }
+    /**
+     * Unification Integer And String
+     * @author Gor Aleksanyan
+     * */
+    public static void unificationIntegerAndString(){
+        int num1 = 8;
+        int num2 = 2;
+        int sum = num1+num2;
+        System.out.println("unification integer and string: "+num1+"+"+num2+"="+sum);
+    }
+    /**
+     * String initialize
+     * @author Gor Aleksanyan
+     * */
+    public static void stringInitialize(){
+        String text1 = "Gor";
+        String text2 = " ";
+        String text3 = "Aleksanyan";
+        System.out.println("Strings initialize: "+text1+text2+text3);
+    }
+    /**
+     * Int Corresponding Char
+     * @author Gor Aleksanyan
+     * */
+    public static void intCorrespondingChar(){
+        Scanner num = new Scanner(System.in);
+        System.out.print("Enter int number(range 32 to 127 ): ");
+        int number = num.nextInt();
+        char sim = (char)number;
+        System.out.println("char: "+sim);
 
+    }
+    /**
+     * Average letters
+     * @author Gor Aleksanyan
+     * */
+    public static void averageLetters(){
+        char g = 'G';
+        char o = 'o';
+        char r = 'r';
+        float av = (g+o+r)/3f;
+        System.out.println("Average letters: "+av);
+    }
+    /**
+     * Cast char to int
+     * @author Gor Aleksanyan
+     * */
+    public static void charToInt(){
+        char a = '5';
+        int b = (int)a;
+        System.out.println(b);
+    }
+    /**
+     *  Name and year of birth using only
+     * char primitive and int types
+     * @author Gor Aleksanyan
+     * */
+    public static void nameAndYearOfBirth(){
+        char g = 71;
+        char o = 111;
+        char r = 114;
+        int year = 1997;
+        System.out.println(g+""+o+""+r+" "+year);
+
+    }
+    /**
+     * Expression 1/3
+     * @author Gor Aleksanyan
+     * */
+    public static void expression(){
+        double a = 1/3d;
+        System.out.println("Double 1/3: "+a);
+        float e =  1/3f; //need to ask
+        System.out.println("Float 1/3: "+e);
+
+    }
+    /**
+     * Circle area and perimeter
+     * @author Gor Aleksanyan
+     * */
+    public static void circle(){
+        Scanner num = new Scanner(System.in);
+        System.out.print("Enter circle radius: ");
+        double r = num.nextDouble();
+        double pi = 3.14159;
+        double area = pi*r*r;
+        double perimeter = 2*pi*r;
+        System.out.println("Circle area: "+area);
+        System.out.println("Circle perimeter: "+perimeter);
+    }
     /**
      * Fahrenheit to Celsius
      * @author Gor Aleksanyan
@@ -61,11 +177,32 @@ public class HomeWorkDay3 {
     }
     public static void main(String[] args) {
         implicit();
+
         explicit();
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Fahrenheit: ");
         float num = sc.nextFloat();
-
         fahrenheitToCelsius(num);
+
+        circle();
+
+        expression();
+
+        nameAndYearOfBirth();
+
+        charToInt();
+
+        averageLetters();
+
+        intCorrespondingChar();
+
+        stringInitialize();
+
+        unificationIntegerAndString();
+
+        stringProblem();
+
+        swapNumbers();
     }
 }
